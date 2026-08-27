5610a383ec6cbc16a1ff81873bb43d49696bcf1b
Moin928 <moinqureshi928@gmail.com>
2026-08-28 00:01:59 +0530

    refactor: improve formatting and organization in CrawlerConfig

--- src/main/java/com/webprobe/config/CrawlerConfig.java
+++ src/main/java/com/webprobe/config/CrawlerConfig.java
@@ -9,11 +9,21 @@ public class CrawlerConfig {
 
     // these values describe how the whole crawler should behave
     private final int maxPages;
+
     private final int maxDepth;
+
     private final int workerCount;
+
     private final int connectionTimeout;
+
     private final int requestTimeout;
+
     private final String userAgent;
+
     private final boolean respectRobots;
+
     private final int delayMs;
+
+    // stops very large http responses from eating up all the memory
+    private final int maxResponseSize;
 }
