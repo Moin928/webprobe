13ee07686a3d35372551e0cf2f837988bb747928
Moin928 <moinqureshi928@gmail.com>
2026-08-28 00:02:45 +0530

    refactor: improve code readability in UrlFrontier by adjusting formatting and adding comments

--- src/main/java/com/webprobe/url/UrlFrontier.java
+++ src/main/java/com/webprobe/url/UrlFrontier.java
@@ -11,24 +11,35 @@ public class UrlFrontier {
     // this queue is shared by all workers so it has to be thread safe
     private final BlockingQueue<UrlTask> queue =
         new LinkedBlockingQueue<>();
+
     private final CrawlLifecycle crawlLifecycle;
 
     public UrlFrontier(CrawlLifecycle crawlLifecycle) {
+
         this.crawlLifecycle = crawlLifecycle;
     }
 
     public void submit(UrlTask task) {
+
+        // tell the lifecycle that new work has been added
+        crawlLifecycle.taskAdded();
 
         // add the task so one of the workers can pick it up
         queue.add(task);
     }
 
     public UrlTask take() throws InterruptedException {
+
         // wait for a little while instead of keeping the thread stuck forever
-        UrlTask task = queue.poll(500, TimeUnit.MILLISECONDS);
+        UrlTask task =
+            queue.poll(500, TimeUnit.MILLISECONDS);
+
         if (task != null) {
-        crawlLifecycle.taskStarted();
+
+            // the worker has taken responsibility for this task
+            crawlLifecycle.taskStarted();
         }
+
         return task;
     }
 
